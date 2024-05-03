fccfeb08f1d2a9c4e86833eca18cf7a677b1365b
ines.garcia <ines.garcia@b-simple.pt>
2024-05-03 15:53:19 +0100

    working

--- projects/holiday/holiday-main/holiday-main.module.ts
+++ projects/holiday/holiday-main/holiday-main.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../src/app/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeComponent,
+    RouterModule.forChild(routes)
   ]
 })
 export class HolidayMainModule { }
--- projects/holiday/src/app/app.routes.ts
+++ projects/holiday/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('../../holiday-main/holiday-main.module').then((m) => m.HolidayMainModule)
+      }
+];
