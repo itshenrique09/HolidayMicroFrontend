9d60f115231d40d57840fdcb66c07cca27d33ab9
Henrique Pereira <henrique.pereira@b-simple.pt>
2024-05-06 10:08:12 +0100

    component and service added default

---
+++ projects/holiday/src/app/lib/component/holiday/holiday-add/holiday-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-holiday-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './holiday-add.component.html',
+  styleUrl: './holiday-add.component.css'
+})
+export class HolidayAddComponent {
+
+}
