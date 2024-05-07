c577afab42783cc44e56966cd6e7775a03a46745
Henrique Pereira <henrique.pereira@b-simple.pt>
2024-05-07 09:55:57 +0100

    path changes

--- projects/holiday/src/app/app.routes.ts
+++ projects/holiday/src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { HolidayAddComponent } from './lib/component/holiday/holiday-add/holiday-add.component';
+import { HolidaysInPeriodComponent } from './lib/component/holiday/holidays-in-period/holidays-in-period.component';
+import { ColabsMoreThanXDaysComponent } from './lib/component/holiday/colabs-more-than-xdays/colabs-more-than-xdays.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'Holiday',
         loadChildren: () => import('../../holiday-main/holiday-main.module').then((m) => m.HolidayMainModule)
     },
+    
+  {
+    path: 'Holiday/add-holiday',
+    component: HolidayAddComponent
+  },
+  {
+    path: 'Holiday/holidays-in-period/:colabId',
+    component: HolidaysInPeriodComponent
+  },
+  {
+    path: 'Holiday/colaborator/:nDays',
+    component: ColabsMoreThanXDaysComponent
+  }
 ];
